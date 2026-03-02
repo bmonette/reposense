9dbec492daa5766d2abd398e395170984c442bd9
bmonette <benoitmonette80@gmail.com>
2026-03-02 13:25:28 -0500

    Add build_parser() to cli.py

--- reposense/cli.py
+++ reposense/cli.py
@@ -7,3 +7,26 @@
 # - Calls the scanner.
 # - Triggers report generation.
 # - Displays results.
+
+import argparse
+from pathlib import Path
+
+
+def build_parser():
+    '''
+    Builds the command line argument parser.
+    '''
+
+    # Creates the ArgumentParser object.
+    parser = argparse.ArgumentParser(description='RepoSense is a local project repository analyzer that generates analysis reports')
+
+    parser.add_argument("scan", type=Path, help="Path to the repository to scan")
+    parser.add_argument("--ignore", type=Path, nargs="+", metavar="PATTERN", help="Ignore the specified extensions")
+    parser.add_argument("--json", action="store_true", help="Output results in JSON format")
+    parser.add_argument("--max-size", type=int, help="Flag large files larger than this size in bytes")
+
+    return parser
+
+
+if __name__ == "__main__":
+    raise SystemExit(build_parser())
